4b5d9ea5aa271673ff1ab8f36c1c9dd17047ff9d
razan kurouni <razankr07@gmail.com>
2024-11-07 19:14:35 +0300

    Resolved merge conflicts and finalized merge

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,14 @@
-<<<<<<< Updated upstream
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+
+import { faBars, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-=======
-
->>>>>>> Stashed changes
 
 const NavBar = () => {
   return (
     <div>
       Navbar
       <FontAwesomeIcon style={{width:'30px'}} icon={faThumbsUp} />
+      <FontAwesomeIcon icon={faBars}  style={{width:'30px'}}/>
     </div>
   )
 }
